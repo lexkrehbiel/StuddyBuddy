e3fb5089c9ff371e8cb53418b32da5db10401821
SHartwick <SHartwick@users.noreply.github.com>
2017-11-21 02:09:52 -0500

    manager fix

--- functions/card_manager.js
+++ functions/card_manager.js
@@ -1,43 +1,43 @@
 
 // pull in the card data from a specified JSON file
 var fs = require('fs');
-var collection = JSON.parse(fs.readFileSync('quiz.json', 'utf8'));
-var deck = collection[0];
+var decks = JSON.parse(fs.readFileSync('quiz.json', 'utf8'));
 
 // initially, set to just before the first card
 var current = -1;
+var num = 0;
 
 // question accessor
 exports.getCurrentQuestion = function(){
-  return deck.cards[current].question;
+  return (decks[num]).cards[current].question;
 }
 
 // answer accessor
 exports.getCurrentAnswer = function(){
-  return deck.cards[current].answer;
+  return (decks[num]).cards[current].answer;
 }
 
 // hint accessor
 exports.getCurrentHint = function(){
-  return deck.cards[current].hint;
+  return (decks[num]).cards[current].hint;
 }
 
 // title accessor
 exports.getCurrentTitle = function(){
-  return deck.title;
+  return (decks[num]).title;
 }
 
 // subject accessor
 exports.getCurrentSubject = function(){
-  return deck.subject;
+  return (decks[num]).subject;
 }
 
 // increment the cursor to move to the next card
 exports.goToNextCard = function(){
-  current = (current + 1) % deck.cards.length;
+  current = (current + 1) % (decks[num]).cards.length;
 }
 
 // set the current deck
 exports.setDeck = function(){
-  deck = collection[0];
+  num = 0;
 }
